d5592a6c3a464f421196a8ba6bf090ceece55868
Joachim chisom <joachimchisom1@gmail.com>
2021-09-08 03:42:44 +0100

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,10 @@
+// GLOBALS - NO WINDOWS!!!!
+
+// _dirname    -path to current directory
+// filename      -filename
+// require        -functon to use (commonJS)
+// module        -Info about current module (file)
+// process        -infor about env where the program i s being executed
+setInterval(() => {
+    console.log('hello world');
+}, 1000)
---
+++ 4-name.js
@@ -0,0 +1,7 @@
+//  Local
+const secret = 'SUPER SECRET'
+//  Share
+const chisom = 'Chisom'
+const ibe = 'Ibe'
+
+module.exports = {chisom,ibe}
